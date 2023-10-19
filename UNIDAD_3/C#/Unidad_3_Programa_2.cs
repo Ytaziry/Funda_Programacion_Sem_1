0a22a77f329dd32def4968a1e8674405787fbf15
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-10-18 21:55:09 -0400

    Add files via upload

---
+++ UNIDAD_3/C#/Unidad_3_Programa_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Unidad_3_Programa_2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cal1, cal2, cal3, cal4, cal5, cal6, promedio;
+
+            Console.WriteLine("Escribe cal 1: ");
+            cal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe cal 2: ");
+            cal2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe cal 3: ");
+            cal3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe cal 4: ");
+            cal4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe cal 5: ");
+            cal5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe cal 6: ");
+            cal6 = Convert.ToInt32(Console.ReadLine());
+            promedio = (cal1 + cal2 + cal3 + cal4 + cal5 + cal6) / 6;
+            Console.WriteLine("Tu promedio es: " + promedio);
+            if (promedio >= 70)
+                Console.WriteLine("Aprovado");
+            else
+                Console.WriteLine("Reprovado");
+        }
+    }
+}
---
+++ UNIDAD_3/C#/Unidad_3_Programa_5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Unidad_3_Programa_5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, p3, total;
+            Console.WriteLine("Escribe precio 1: ");
+            p1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe precio 2: ");
+            p2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe precio 3: ");
+            p3 = Convert.ToDouble(Console.ReadLine());
+            total = p1 + p2 + p3;
+            if (total >= 1500)
+            {
+                total = total - (total * 0.30);
+                Console.WriteLine("El total (30% desc): " + total);
+            }
+            else if (total < 1500 && total >= 1000)
+            {
+                total = total - (total * 0.20);
+                Console.WriteLine("El total (20% desc): " + total);
+            }
+            else if (total < 1000 && total >= 700)
+            {
+                total = total - (total * 0.10);
+                Console.WriteLine("El total (10% desc): " + total);
+            }
+            else
+            {
+                Console.WriteLine("El total del descuento es de 0%: " + total);
+
+            }
+        }
+    }
+}
