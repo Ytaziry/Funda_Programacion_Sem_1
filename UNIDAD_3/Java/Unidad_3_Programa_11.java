465a51c8388c7781f164834012fd0c9200a30c73
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-10-24 19:11:59 -0600

    Add files via upload

---
+++ UNIDAD_3/Java/Unidad_3_Programa_11.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unidad_3_programa_11;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Unidad_3_Programa_11 {
+
+    public static void main(String[] args) {
+        for ( int c = 1; c<= 100; c++){
+        System.out.println(c);
+        }
+        for ( int i=100; i>=1; i--){
+        System.out.println(i);    
+        }
+}
+}
+                                                                                     
