9a96833b2cd098bb0c1073ebc36963309c413056
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-11-01 19:52:20 -0600

    Add files via upload

---
+++ UNIDAD_3/Java/Unidad_3_Programa_23.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unida_3_programa_23;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Unida_3_Programa_23 {
+
+    public static void main(String[] args) {
+     int numero = 1;
+     do { 
+         System.out.println(numero);
+         numero++;
+     }while (numero <=10);
+        
+    }
+}
+
