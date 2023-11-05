a1484456abad90418ae1633aa2a521abbf844f7e
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-11-05 00:39:40 -0600

    Add files via upload

---
+++ UNIDAD_3/Java/Unidad_3_Programa_14.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unidad_3_programa_14;
+
+/**
+ *
+ * @author USUARIO LENOVO
+ */
+public class Unidad_3_Programa_14 {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 200; i += 2 ){
+        System.out.println(i);
+        }
+    }
+}
