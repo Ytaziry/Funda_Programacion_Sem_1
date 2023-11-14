ca47f23b49fd6ad469487a4ae969346b1ed88e43
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-11-13 19:55:58 -0600

    Add files via upload

---
+++ Unidad 4/Java/Prog_11_Uni_4.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog_11_uni_4;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Prog_11_Uni_4 {
+
+    public static void main(String[] args) {
+       /* int [][] calif = new int [3][3];
+        //calif [fila][columa]
+        calif [0][0]=5;
+        calif [0][1]=7;
+        calif [0][2]=9;
+        calif [1][0]=6;
+        calif [1][1]=8;
+        calif [1][2]=7;
+        calif [2][0]=10;
+        calif [2][1]=3;
+        calif [2][2]=1;    */
+        int [][] calif ={{5,7,9},
+                         {6,8,7},  
+                         {10,3,1}};                    
+        System.out.print(calif[0][0]);
+        System.out.print(calif[0][1]);
+        System.out.println(calif[0][2]);
+        System.out.print(calif[1][0]);
+        System.out.print(calif[1][1]);
+        System.out.println(calif[1][2]);
+        System.out.print(calif[2][0]);
+        System.out.print(calif[2][1]);
+        System.out.print(calif[2][2]);
+    }
+} 
