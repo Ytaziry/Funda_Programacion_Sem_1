c5f33ee13c3a201e25505bac52853d403b033e75
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-11-13 19:13:40 -0600

    Add files via upload

---
+++ Unidad 4/C#/Prog_3Uni_4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f = 0, k = 0;
+            int Ttemp;
+            Console.WriteLine("Escribe el total de Temperaturas: ");
+            Ttemp = Convert.ToInt32(Console.ReadLine());
+
+            double[] TempC = new double[Ttemp];
+
+            for (int i = 0; i < Ttemp; i++)
+            {
+                Console.WriteLine("Ingrese Temperatura en índice" + i + ": ");
+                TempC[i] = Convert.ToDouble(Console.ReadLine());
+
+                f = (TempC[i] * 9 / 5) + 32;
+                Console.WriteLine("Los grados Farenheit son: " + f);
+
+                k = TempC[i] + 273.15;
+                Console.WriteLine("Los grados Kelvin son: " + k);
+            }
+            
+        }
+    }
+}
