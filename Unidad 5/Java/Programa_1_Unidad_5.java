8cd7b61294a75db90da1171d13af399c3d5c68af
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-11-30 19:56:33 -0600

    Add files via upload

---
+++ Unidad 5/Java/Programa_1_Unidad_5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa_1_unidad_5;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa_1_Unidad_5 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Suma=" + suma (7,8));
+    }
+    
+    //Métoso o Función
+    public static int suma (int a, int b)
+    {
+        int r = a + b;
+        return r;
+    }
+}
