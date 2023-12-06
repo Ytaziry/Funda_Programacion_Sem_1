9f00af422c718762780ca561e913d86aeb191d90
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-12-06 00:14:50 -0600

    Add files via upload

---
+++ Unidad 4/C#/Prog_2_Uni_4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prog_2_Uni_4 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] calificaciones = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Ingrese la calificación {i + 1}: ");
+                calificaciones[i] = double.Parse(Console.ReadLine());
+            }
+
+            double suma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                suma += calificaciones[i];
+            }
+
+            double promedio = suma / 10;
+
+            Console.WriteLine($"El promedio de las calificaciones es: {promedio}");
+        }
+    }
+}
