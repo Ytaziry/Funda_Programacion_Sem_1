c4eb962f5a404c6ae18299b8a655f4911a67953c
Ytaziry <146400677+Ytaziry@users.noreply.github.com>
2023-12-06 00:15:35 -0600

    Add files via upload

---
+++ Unidad 4/C#/Prog_4_Uni_4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prog_4_Uni_4 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 5, -2, 10, -8, 3, -1, 7 };
+
+
+            int totalN = ContarN(numeros);
+
+
+            Console.WriteLine($"El total de números negativos es: {totalN}");
+        }
+
+        static int ContarN(int[] array)
+        {
+            int contador = 0;
+
+
+            foreach (int numero in array)
+            {
+                if (numero < 0)
+                {
+                    contador++;
+                }
+            }
+
+            return contador;
+        }
+    }
+}
---
+++ Unidad 4/C#/Prog_5_Uni_4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Prog_5_Uni_4 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "Manzana", "Fresa", "Cereza", "Frambuesa", "Granada" };
+
+
+            string[] frutas_AlReves = new string[frutas.Length];
+
+
+            for (int i = 0; i < frutas.Length; i++)
+            {
+                frutas_AlReves[i] = frutas[frutas.Length - 1 - i];
+            }
+
+
+            Console.WriteLine("Arreglo Original:");
+            ImprimirArreglo(frutas);
+
+
+            Console.WriteLine("\nArreglo al Revés:");
+            ImprimirArreglo(frutas_AlReves);
+        }
+
+        static void ImprimirArreglo(string[] array)
+        {
+            foreach (string elemento in array)
+            {
+                Console.Write($"{elemento} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
